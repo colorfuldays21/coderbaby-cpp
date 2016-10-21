76725c6183e49ab30370da3595019127b71be67c
[earth]lolo@ <colorfuldays21@aliyun.com>
2016-10-21 21:47:31 +0800

    test

---
+++ make.sh
@@ -0,0 +1,27 @@
+#ubuntu16.04 tasks.json c/c++ 后缀名匹配 库包含匹配bash脚本
+#$1->${fileBasename} $2->${fileExtname}
+#2016.10.21 by:coderbaby
+if [ ".c" = $2 ]; then 
+    grep -q "gtk/gtk.h" $1
+    if [ 0 = $? ]; then
+        echo "gcc -g -O2 -std=gnu11 $1 -o `basename $1 .c` -lm `pkg-config --cflags --libs gtk+-3.0`" &&
+        gcc -g -O2 -std=gnu11 $1 -o `basename $1 .c` -lm `pkg-config --cflags --libs gtk+-3.0` &&
+        gnome-terminal -x bash -c "./`basename $1 .c`;read -n1 -p '请按任意键继续. . .'"
+    else
+        echo "gcc -g -O2 -std=gnu11 -static $1 -o `basename $1 .c` -lm" &&
+        gcc -g -O2 -std=gnu11 -static $1 -o `basename $1 .c` -lm &&
+		gnome-terminal -x bash -c "./`basename $1 .c`;read -n1 -p '请按任意键继续. . .'"
+    fi
+fi
+if [ ".cpp" = $2 ]; then
+    grep -q "gtk/gtk.h" $1
+    if [ 0 = $? ]; then
+        echo "g++ -g -O2 -std=gnu++14 $1 -o `basename $1 .cpp` `pkg-config --cflags --libs gtk+-3.0`" &&
+        g++ -g -O2 -std=gnu++14 $1 -o `basename $1 .cpp` `pkg-config --cflags --libs gtk+-3.0` &&
+		gnome-terminal -x bash -c "./`basename $1 .cpp`;read -n1 -p '请按任意键继续. . .'"
+    else
+        echo "g++ -g -O2 -std=gnu++14 -static $1 -o `basename $1 .cpp`" &&
+        g++ -g -O2 -std=gnu++14 -static $1 -o `basename $1 .cpp` &&
+		gnome-terminal -x bash -c "./`basename $1 .cpp`;read -n1 -p '请按任意键继续. . .'"
+    fi
+fi
---
+++ test.cpp
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<GL/glut.h>
+void myFunc(void);
+void myInit(void);
+void mySize(GLsizei,GLsizei);
+int main(int argc, char* argv[])    
+{    
+    glutInit(&argc, argv);    
+    glutInitDisplayMode(GLUT_RGBA | GLUT_SINGLE);
+	glutInitWindowSize(640,480);
+    glutCreateWindow("640x480");    
+    glutDisplayFunc(myFunc);
+	glutReshapeFunc(mySize);
+	myInit();
+    glutMainLoop();    
+    return 0;    
+} 
+void myFunc(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	//glRectf(-25.0f,25.0f,25.0f,-25.0f);
+	glShadeModel(GL_SMOOTH);
+	glBegin(GL_TRIANGLES);
+		glColor3ub((GLubyte)255,(GLubyte)0,(GLubyte)0);
+		glVertex2f(0,0);
+		glColor3ub((GLubyte)0,(GLubyte)255,(GLubyte)0);
+		glVertex2f(639,0);
+		glColor3ub((GLubyte)0,(GLubyte)0,(GLubyte)255);
+		glVertex2f(0,479);
+		//
+		//
+		glColor3ub((GLubyte)0,(GLubyte)255,(GLubyte)0);
+		glVertex2f(639,0);
+		glColor3ub((GLubyte)255,(GLubyte)0,(GLubyte)0);
+		glVertex2f(639,479);
+		glColor3ub((GLubyte)0,(GLubyte)0,(GLubyte)255);
+		glVertex2f(0,479);
+	glEnd();
+	glFlush();
+}
+void myInit(void)
+{
+	glClearColor(0.0f,0.0f,0.0f,1.0f);
+	//glColor3f(1.0f,0.0f,0.0f);
+}
+void mySize(GLsizei w,GLsizei h)
+{
+	//printf("w=%d\th=%d\n",w,h);
+	glViewport(0,0,w,h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0,w-1,h-1,0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
